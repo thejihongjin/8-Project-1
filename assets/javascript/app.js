7c8a8f71bdc0a21b6dc161f2691695c80a4e19ba
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-04-13 14:31:42 -0700

    added meme api, added secondary api call function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 
 
 var queryURL = '';
+var userInput = '';
 generateAPIs();
 
 function generateAPIs() { // generate api divs & buttons
@@ -15,12 +16,6 @@ function generateAPIs() { // generate api divs & buttons
             name: 'chucknorris',
             url: 'http://api.icndb.com/jokes/random',
             text: 'Chuck Norris Facts',
-            input: 'n'
-        },
-        {
-            name: 'chucknorrisio',
-            url: 'https://api.chucknorris.io/jokes/random',
-            text: 'Chuck Norris Facts 2',
             input: 'n'
         },
         {
@@ -43,9 +38,9 @@ function generateAPIs() { // generate api divs & buttons
         },
         {
             name: 'giphy',
-            url: 'https://api.giphy.com/v1/gifs/random?api_key=2D9ZWdGSO6zZOnd7dqwMAxdeeDM0Bp1I', // /search?api_key=2D9ZWdGSO6zZOnd7dqwMAxdeeDM0Bp1I&limit=10' //&q=',
+            url: 'https://api.giphy.com/v1/gifs/random?api_key=2D9ZWdGSO6zZOnd7dqwMAxdeeDM0Bp1I',
             text: 'Give me a random GIF',
-            input: 'y'
+            input: 'y',
         },
         {
             name: 'icanhazdadjoke',
@@ -67,8 +62,8 @@ function generateAPIs() { // generate api divs & buttons
         },
         {
             name: 'memegenerator',
-            url: 'https://www.reddit.com/r/memes.json?sort=top', //not quite working yet
-            text: 'Memerator',
+            url: 'https://meme-api.herokuapp.com/gimme',
+            text: 'Meme Me Up, Scotty',
             input: 'n'
         },
         {
@@ -109,7 +104,7 @@ function generateAPIs() { // generate api divs & buttons
             form.append(input);
 
             var submit = $("<button>");
-            submit.addClass("api-btn"); //change to submit onclick?
+            submit.addClass("api-btn");
             submit.attr("data-name", apiArr[i].name);
             submit.attr("data-url", apiArr[i].url);
             submit.text(apiArr[i].text);
@@ -129,15 +124,23 @@ function generateAPIs() { // generate api divs & buttons
     };
 }
 
-
 $(document).on("click", ".api-btn", function () {
+    event.preventDefault();
+
     apiName = $(this).attr("data-name");
     queryURL = $(this).attr("data-url");
 
-    if (apiName === 'translateminion' || apiName === 'translateyoda') {
+    if (apiName === 'giphy') {
+        userInput = $('#giphy-input').val().trim();
+        if (userInput !== '') {
+            apiName += '-input';
+            queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=2D9ZWdGSO6zZOnd7dqwMAxdeeDM0Bp1I&q="' + userInput + '"&limit=100';
+        }
+        mainAPIrequest();  
+    } else if (apiName === 'translateminion' || apiName === 'translateyoda') {
         inputField();
     } else {
-        testAPI();  
+        mainAPIrequest();  
     }
 });
 
@@ -155,20 +158,20 @@ $(document).on("click", ".giphy-img", function () {
 
 function inputField() {
     console.log(apiName)
-    $('#exampleModal').modal('show');
+    $('#translate-modal').modal('show');
     
-    $(document).on('click', '#input-submit', function(){
-        userInput = $('#message-text').val().trim()
+    $(document).on('click', '#modal-submit', function(){
+        userInput = $('#modal-input').val().trim()
         $("#api-result").empty();
         $.getJSON(queryURL + userInput, function(data) {
             console.log(data)   
             $('#api-result').html('<p>' + data.contents.translated + '</p>');       
         })
-        $('#exampleModal').modal('hide')
+        $('#translate-modal').modal('hide')
     })
 }
 
-function testAPI() {
+function mainAPIrequest() {
     $.ajax({
         url: queryURL,
         method: "GET",
@@ -179,29 +182,34 @@ function testAPI() {
         console.log(response);
 
         $("#api-result").empty();
+
         switch (apiName) {
             case 'bored':
-                $("#api-result").text(response.activity + ".")
+                $("#api-result").html("<p>" + response.activity + "</p>")
                 break;
 
             case 'chucknorris':
-                $("#api-result").html('<p>' + response.value.joke + '</p>')
-                break;
-
-            case 'chucknorrisio':
-                $("#api-result").html('<p>' + response.value + '</p>')
+                $("#api-result").html("<p>" + response.value.joke + "</p>")
+                queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=2D9ZWdGSO6zZOnd7dqwMAxdeeDM0Bp1I&q="Chuck+Norris"&limit=100';
+                secondAPIrequest();
                 break;
 
             case 'dogimg':
-                $("#api-result").html('<img src=' + response.message + '>')
+                $("#api-result").append('<img src=' + response.message + '>')
                 break;
 
             case 'fortunecookie':
-                // $("#api-result").text();
+                var fortuneIndex = parseInt(Math.floor(Math.random() * response.length));
+                var results = response[fortuneIndex];
+                $("#api-result").html("<p>" + results.fortune + "</p>");
                 break;
 
             case 'geek':
-                // $("#api-result").text();
+                if (response.toLowerCase().includes("chuck norris")) {
+                    mainAPIrequest();
+                } else {
+                    $("#api-result").html("<p>" + response + "</p>");
+                }
                 break;
 
             case 'giphy':
@@ -215,28 +223,44 @@ function testAPI() {
                 $("#api-result").append(giphyImg);
                 break;
 
+            case 'giphy-input':
+                var giphyIndex = parseInt(Math.floor(Math.random() * response.data.length));
+                var results = response.data[giphyIndex];
+                var giphyImg = $("<img>");
+                giphyImg.attr("src", results.images.fixed_height.url);
+                giphyImg.addClass("giphy-img");
+                giphyImg.attr("data-state", "animate");
+                giphyImg.attr("data-still", results.images.fixed_height_still.url);
+                giphyImg.attr("data-animate", results.images.fixed_height.url);
+                $("#api-result").append(giphyImg);
+                break;
+
             case 'icanhazdadjoke':
-                $("#api-result").text(response.joke);
+                $("#api-result").html("<p>" + response.joke + "</p>");
                 break;
 
             case 'jokes':
-                $("#api-result").text(response.setup + " " + response.punchline);
+                $("#api-result").html("<p>" + response.setup + "<br>" + response.punchline + "</p>");
                 break;
 
             case 'kanye':
-                $("#api-result").text(response.quote);
+                $("#api-result").html('<p>Kanye says:<br>"' + response.quote + '"</p>');
+                queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=2D9ZWdGSO6zZOnd7dqwMAxdeeDM0Bp1I&q="Kanye+West"&limit=100';
+                secondAPIrequest();
                 break;
 
             case 'memegenerator':
-                // $("#api-result").text();
+                $("#api-result").append('<img src=' + response.url + ' class="meme-img">');
                 break;
 
             case 'randomcats':
-                $("#api-result").html('<img src=' + response[0].url + '>')
+                $("#api-result").append('<img src=' + response[0].url + '>')
                 break;
 
             case 'ronswanson':
-                $("#api-result").text(response[0]);
+                $("#api-result").html('<p>Ron Swanson says:<br>"' + response[0] + '"</p>');
+                queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=2D9ZWdGSO6zZOnd7dqwMAxdeeDM0Bp1I&q="Ron+Swanson"&limit=100';
+                secondAPIrequest();
                 break;
 
             case 'translateminion': //done in inputField function
@@ -255,7 +279,29 @@ function testAPI() {
     });
 }
 
-
+function secondAPIrequest() {
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        headers: {
+            Accept: "application/json"
+        }
+    }).then(function (response) {
+        // console.log(response);
+    
+        var randomIndex = parseInt(Math.floor(Math.random() * response.data.length));
+        // console.log(randomIndex);
+        var results = response.data[randomIndex];
+        var giphyImg = $("<img>");
+        giphyImg.attr("src", results.images.fixed_height.url);
+        giphyImg.addClass("giphy-img");
+        giphyImg.attr("data-state", "animate");
+        giphyImg.attr("data-still", results.images.fixed_height_still.url);
+        giphyImg.attr("data-animate", results.images.fixed_height.url);
+        $("#api-result").append("<br>");
+        $("#api-result").append(giphyImg);
+    });
+}
 
 
 
@@ -291,5 +337,5 @@ connectedRef.on("value", function (snap) { // when the client's connection state
 });
 
 connectionsRef.on("value", function (snapshot) { // when first loaded or when the connections list changes...
-    $("#watchers").text(snapshot.numChildren());
+    $("#watchers").html(snapshot.numChildren());
 });
